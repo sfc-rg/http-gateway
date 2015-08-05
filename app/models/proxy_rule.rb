5e4b0fb1e317974a4ce2730a730e33c6cf5cb042
Yusei Yamanaka <yusei1128@gmail.com>
2015-08-05 19:08:27 +0900

    Fix regex

--- app/models/proxy_rule.rb
+++ app/models/proxy_rule.rb
@@ -1,5 +1,5 @@
 class ProxyRule < ActiveRecord::Base
-  VALID_DOMAIN_REGEX = /\A(?:[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]|[A-Za-z0-9])\Z/
+  VALID_DOMAIN_REGEX = /\A(?:[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]|[A-Za-z0-9])\z/
 
   belongs_to :user
 
