d40d758503f82207759808396c19bda8b14b5ffc
Yusei Yamanaka <yusei1128@gmail.com>
2015-08-06 07:56:07 +0900

    Fix cookie session store domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 if Rails.env.production?
-  Rails.application.config.session_store :cookie_store, key: '_http-gateway_session', domain: '.sfc.widead.jp'
+  Rails.application.config.session_store :cookie_store, key: '_http-gateway_session', domain: '.' + Domain::BASE_DOMAIN
 else
   Rails.application.config.session_store :cookie_store, key: '_http-gateway_session'
 end
