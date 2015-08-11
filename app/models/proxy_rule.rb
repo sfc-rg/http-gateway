75642693689beefedc67a8903b03b630a2878846
Yusei Yamanaka <yusei1128@gmail.com>
2015-08-12 08:49:52 +0900

    Add target url validation within wide cloud ip ranges

--- app/models/proxy_rule.rb
+++ app/models/proxy_rule.rb
@@ -1,11 +1,19 @@
 class ProxyRule < ActiveRecord::Base
   VALID_DOMAIN_REGEX = /\A(?:[A-Za-z0-9][A-Za-z0-9\-]{0,61}[A-Za-z0-9]|[A-Za-z0-9])\z/
+  TARGET_IPADDR_RANGE = '2001:200:0:889c::/64'
 
   belongs_to :user
 
   validates :name, presence: true
   validates :domain, format: { with: VALID_DOMAIN_REGEX }, uniqueness: true
   validates :url, format: { with: /\Ahttps?:/ }
+  validate do
+    require 'addressable/uri'
+    target_addr = IPAddr.new(Addressable::URI.parse(self.url).hostname)
+    unless IPAddr.new(TARGET_IPADDR_RANGE).include?(target_addr)
+      errors.add(:url, :has_invalid_target)
+    end
+  end
   enum auth_type: { open: 0, intranet: 1, slack_auth: 2 }
 
   scope :active, -> { where('expired_at IS NULL OR expired_at < ?', Time.now) }
